a84bde6a3daa5f034bb58ee70fb319e7391b1f36
Santiago Vallejo <saniagovallejoc@gmail.com>
2017-12-07 09:52:03 -0500

    change seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,14 +6,3 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-a = Url.new
-a.original = 'https://github.com/alexkalik1'
-a.save
-
-b = Url.new
-b.original = 'https://twitter.com/alex_kalik'
-b.save
-
-c = Url.new
-c.original = 'https://twitter.com/jciancio7'
-c.save
